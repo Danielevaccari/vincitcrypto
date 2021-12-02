30a0337ad97444d486fe9cc2098996739fb41611
Daniele Vaccari <daniele.j.vaccari@gmail.com>
2021-12-02 12:32:09 +0200

    initial commit, made project and little setup

---
+++ src/components/cryptoanalytics/Analytics.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <>
+      <div className="cryptotrackercontainer">
+        juuuui
+      </div>
+    </>
+  )
+}
+
+export default Analytics
